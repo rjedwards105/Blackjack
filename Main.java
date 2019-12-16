85d8fd78447734281ccd6f9988d8f393cc764988
rjedwards105 <54290212+rjedwards105@users.noreply.github.com>
2019-12-15 20:40:10 -0800

    done
    done

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main implements Runnable 
+{
+
+    long xTime = System.nanoTime();
+    public static boolean terminator = false;
+    public static int pWins = 0;
+    public static int dWins = 0;
+
+    //screen refresh rate
+    public int Hz = 100;
+
+    GUI gui = new GUI();
+
+    public static void main(String[] args) {
+        new Thread(new Main()).start();
+    }
+
+    @Override
+    public void run() {
+        while(terminator == false) {
+            if (System.nanoTime() - xTime >= 1000000000/Hz) {
+                gui.refresher();
+                gui.repaint();
+                xTime = System.nanoTime();
+            }
+        }
+    }
+}
