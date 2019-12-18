133b3908ddba65eaa903621051c8cab10e2e1642
rjedwards105 <54290212+rjedwards105@users.noreply.github.com>
2019-12-17 23:26:26 -0800

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,7 +1,7 @@
 public class Message
 {
-    String mes = "empty_for_now";
-    String who = "nobody_for_now";
+    String mes = "empty_for_now"; //temporary string holder for in-game messages
+    String who = "nobody_for_now"; //temporary string holder for in-game messages
 
     public Message(String m, String w) {
         this.mes = m;
